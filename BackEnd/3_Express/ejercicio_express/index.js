842689ac082b1091ba6768313415f4cf00e562fb
German <gerfersie@gmail.com>
2022-01-17 12:53:03 +0100

    Building all endpoints

--- BackEnd/3_Express/ejercicio_express/index.js
+++ BackEnd/3_Express/ejercicio_express/index.js
@@ -7,6 +7,38 @@ app.get('/',(req,res)=>{
     res.send("Hola esto es un mensaje de bienvenida")
 })
 
+app.get('/Productos',(req,res)=>{
+    res.send("Listado de productos Get")
+})
+
+app.post('/Productos',(req,res)=>{
+    res.send("Listado de productos Post")
+})
+
+app.put('/Productos',(req,res)=>{
+    res.send("Listado de productos Put")
+})
+
+app.delete('/Productos',(req,res)=>{
+    res.send("Listado de productos Delete")
+})
+
+app.get('/Usuarios',(req,res)=>{
+    res.send("Listado de usuarios")
+})
+
+app.post('/Usuarios',(req,res)=>{
+    res.send("Crear un usuario")
+})
+
+app.put('/Usuarios',(req,res)=>{
+    res.send("Actualizar un usuario")
+})
+
+app.delete('/Usuarios',(req,res)=>{
+    res.send("Borrar un usuario")
+})
+
 app.listen(puerto, (err) => {
     if (err) {
         console.error(`No se pudo levantar el servidor en el puerto ${puerto}`)
