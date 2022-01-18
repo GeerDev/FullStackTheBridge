aaa030be7177d61aa78673b9e4535131760f6ee8
German <gerfersie@gmail.com>
2022-01-18 12:19:46 +0100

    Create Database and Tables Express SQL

--- BackEnd/4_Express_SQL/ejercicio_express_sql/index.js
+++ BackEnd/4_Express_SQL/ejercicio_express_sql/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const mysql = require('mysql2');
+require('dotenv').config()
 
 app.use(express.json())
+
+console.log(process.env);
 
 const db = mysql.createConnection({
   host     : 'localhost',
@@ -13,6 +16,7 @@ const db = mysql.createConnection({
 
 db.connect();
 
+// Creamos la Base de Datos
 app.get('/createdb',(req,res)=>{
     let sql ='CREATE DATABASE expressDB';
     db.query(sql,(err,result)=>{
@@ -21,6 +25,35 @@ app.get('/createdb',(req,res)=>{
       res.send('Database created...')
     })
   }) 
+
+// Creamos las tablas
+app.get('/createtableproducts',(req,res)=>{
+    let sql = 'CREATE TABLE expressDB.products(id INT AUTO_INCREMENT,name_product VARCHAR(50),price INT,PRIMARY KEY(id))'
+      db.query(sql,(err,result)=> {
+        if(err) throw err;
+        console.log(result);
+        res.send('Posts table products created...')
+      })
+    })
+
+app.get('/createtablecategories',(req,res)=>{
+    let sql = 'CREATE TABLE expressDB.categories(id INT AUTO_INCREMENT,name_category VARCHAR(50),_description VARCHAR(50),PRIMARY KEY(id))'
+        db.query(sql,(err,result)=> {
+          if(err) throw err;
+          console.log(result);
+          res.send('Posts table categories created...')
+        })
+    })
+
+app.get('/createtableproductscategories',(req,res)=>{
+    let sql = 'CREATE TABLE expressDB.productoscategorias(id INT AUTO_INCREMENT,product_id INT,category_id INT,PRIMARY KEY(id),FOREIGN KEY(product_id) REFERENCES expressDB.products(id),FOREIGN KEY(category_id) REFERENCES expressDB.categories(id))'
+        db.query(sql,(err,result)=> {
+          if(err) throw err;
+          console.log(result);
+          res.send('Posts table categories created...')
+        })
+    })
+
 
 app.listen(5000,()=>{
     console.log('servidor levantado en el puerto 5000')
